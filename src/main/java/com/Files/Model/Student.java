4809709d9a1d5bf7764bbb58d01c07d5b1eb1f56
Siddharth <siddharthparmar.dev@gmail.com>
2026-07-11 23:55:01 +0530

    First commit

---
+++ src/main/java/com/Files/Model/Student.java
@@ -0,0 +1,44 @@
+package com.Files.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Student {
+    @Id
+    @Column(name="SID",nullable = false)
+    private int id;
+
+    @Column(name = "SNAME")
+    private String name;
+
+    @Column(name = "SCITY")
+    private String city;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
